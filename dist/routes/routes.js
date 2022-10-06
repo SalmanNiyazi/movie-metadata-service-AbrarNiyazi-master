de764d969017ad76312108b260647899205fe407
technoSalman <107096344+technoSalman@users.noreply.github.com>
2022-10-06 15:57:53 +0530

    Delete dist directory

--- dist/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const movieMetadata_1 = require("../controllers/movieMetadata");
-const router = (0, express_1.Router)();
-router.get('/:id?', movieMetadata_1.movieMetData);
-exports.default = router;
