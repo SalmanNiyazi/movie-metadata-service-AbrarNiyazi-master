de161e8b26d569a25b7ce5d1bf034b56b8bf2236
technoSalman <107096344+technoSalman@users.noreply.github.com>
2022-10-19 13:25:59 +0530

    Update metaDataService.ts

--- src/services/metaDataService.ts
+++ src/services/metaDataService.ts
@@ -1,20 +1,3 @@
-import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
-
-export const dataMerger = async (id: string) => {
-	console.log(id);
-	try {
-		const OMDBData = await axios.get<AxiosResponse>(
-			`https://www.omdbapi.com/?i=${id}&apikey=68fd98ab&plot=full`
-		);
-		console.log(JSON.stringify(OMDBData));
-	} catch (error) {
-		console.log(error);
-		if (axios.isAxiosError(error)) {
-			console.log('error message: ', error.message);
-			return error.message;
-		} else {
-			console.log('unexpected error: ', error);
-			return 'An unexpected error occurred';
-		}
-	}
-};
+import axios from 'axios';
+import fs from 'fs';
+//search for searchObject in here
